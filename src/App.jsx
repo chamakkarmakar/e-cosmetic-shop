099600b4c5e462fb5ac7598771058044b58d9c36
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2023-09-11 11:51:52 +0600

    first commit of e-cosmetic shop

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Banner from './component/Banner/Banner'
+import Header from './component/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors : {
+      'purple': '#def7ff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
